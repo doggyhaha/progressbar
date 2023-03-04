b8a0b3c1b19a9c7fd556c20146158fa76514f65a
dogghi no <75123663+doggyhaha@users.noreply.github.com>
2023-03-04 15:29:58 +0100

    Test fix trailing characters

--- progressbar.go
+++ progressbar.go
@@ -969,8 +969,13 @@ func renderProgressBar(c config, s *state) (int, error) {
 		str = colorstring.Color(str)
 	}
 
+	if c.useANSICodes {
+	// append the "clear rest of line" ANSI escape sequence
+		str = str + "\033[0K"
+	}
+	
 	s.rendered = str
-
+	
 	return getStringWidth(c, str, false), writeString(c, str)
 }
 
